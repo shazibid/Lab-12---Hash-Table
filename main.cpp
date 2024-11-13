11e15cb080165f8fdc7c21e4b686a52682103ac3
shazibid <135661233+shazibid@users.noreply.github.com>
2024-11-13 15:00:00 -0800

    main
    miannnnianin

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Shazi Bidarian on 11/13/24.
+//
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    cout << "hi";
+
+    cout << "whaddafwick";
+    return 0;
+}
