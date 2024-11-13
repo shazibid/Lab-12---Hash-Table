bda6b2dd273788871f384c485bade4340cc4234f
shazibid <135661233+shazibid@users.noreply.github.com>
2024-11-13 15:40:20 -0800

    Update to class

--- Professor_Catalog.h
+++ Professor_Catalog.h
@@ -4,7 +4,27 @@
 
 #ifndef PROFESSOR_CATALOG_H
 #define PROFESSOR_CATALOG_H
+#include <iostream>
+#include <string>
+using namespace std;
 
+class Professor_Catalog {
+private:
+    string prof;    //this is our hashmap key
+    string courseName;  //this is our hashmap value
+    double rating;
+
+public:
+    Professor_Catalog(string prof, string courseName, double rating) {
+        this->prof = prof;
+        this->courseName = courseName;
+        this->rating = rating;
+    }
+
+    ~Professor_Catalog() {}
+
+
+};
 
 
 #endif //PROFESSOR_CATALOG_H
--- main.cpp
+++ main.cpp
@@ -6,24 +6,6 @@
 #include <string>
 using namespace std;
 
-class Professor_Catalog {
-private:
-    string prof;    //this is our hashmap key
-    string courseName;  //this is our hashmap value
-    double rating;
-
-public:
-    Professor_Catalog(string prof, string courseName, double rating) {
-        this->prof = prof;
-        this->courseName = courseName;
-        this->rating = rating;
-    }
-
-    ~Professor_Catalog() {}
-
-
-};
-
 int main()
 {
 
