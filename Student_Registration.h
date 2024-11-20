8b763f999e9cfede4299d5831b4c780341975057
nadia <149120502+nadiazada@users.noreply.github.com>
2024-11-20 10:23:32 -0800

    Created Student_Registration.h

---
+++ Student_Registration.h
@@ -0,0 +1,39 @@
+#ifndef STUDENT_REGISTRATION_H
+#define STUDENT_REGISTRATION_H
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Hash class for student registrations
+class StudentTable {
+private:
+struct Registration { // Keeps us from using two seperate vectors for course and professor
+    string courseName;
+    string professorName;
+
+    Registration(const string& course, const string& professor)
+        : courseName(course), professorName(professor) {}
+};
+
+//struct for storing student info
+struct Student {
+    int studentID;
+    vector<Registration> registeredCourses; //student may have registered for multiple classes, vector allows for flexibility
+
+    Student(int id) : studentID(id) {}
+};
+
+    static const int SIZE = 15; // smaller size to keep things simple
+    vector<vector<Student>> table; //hash table
+    int hashFunction(int studentID) const; //hash function for student
+
+public:
+    StudentTable(); // Constructor
+    void add(int studentID, const string& courseName, const string& professorName, const vector<string>& professors); // Vector - professors for each course (to output to the options)
+    void verify(int studentID);
+    void checkRegistration(int studentID, const string& courseName, const string& professorName) const;
+    void displayRegistrations(int studentID) const;
+};
+
+#endif // STUDENT_REGISTRATION_H
